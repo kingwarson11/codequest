ae2f7fdaffa31961553f39a65bd5cb795768d7a1
Warren <esonwarr@gmail.com>
2026-05-21 18:43:10 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
+// Hide the splash screen once React has mounted
+if (window.__hideSplash) window.__hideSplash()
